0d707e40b14185cfa7fa2631cbbfad9ca120e4b1
Erdal ALTIN <erdalaltin@gmail.com>
2023-01-20 09:50:59 +0300

    Cleaned up the settings a little bit

--- install.sh
+++ install.sh
@@ -244,10 +244,10 @@ if [ -x "$(command -v docker)" ] && [ "$(docker compose version)" ]; then
 				if sudo [ -d "./certbot/live/$domain_name" ]; then break; fi
 			done
 			echo "Ok."
-			until [ ! -z `docker ps -q -f "status=running" --no-trunc | grep $(docker compose ps -q webserver)` ]; do
+			until [ ! -z `docker compose ps -a --filter "status=running" --services | grep webserver` ]; do
 				echo "waiting starting webserver container"
 				sleep 2s & wait ${!}
-				if [ ! -z `docker ps -q -f "status=running" --no-trunc | grep $(docker compose ps -q webserver)` ]; then break; fi
+				if [ ! -z `docker compose ps -a --filter "status=running" --services | grep webserver` ]; then break; fi
 			done			
 			echo ""
 			echo "Reloading webserver ssl configuration"
